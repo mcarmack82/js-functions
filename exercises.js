36242ed684278cfb9334b7cdbd38a75b700d4f7e
mcarmack82 <michelleco@MichellsMacBook.home>
2019-04-05 21:50:36 -1000

    #3 complete

--- exercises.js
+++ exercises.js
@@ -86,6 +86,12 @@ console.log('Difference: ', difference);
  * Console.log `product` to test your code.
 */
 
+function multiply(num1, num2){
+    return num1 * num2;  
+}
+var product = multiply(bango1, bango2);
+console.log('Product: ', product);
+
 
 
 /*
